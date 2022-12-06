e07ceddb508badf49961909b0a01561d427f37fa
Ivans Aļeškevičs <78017578+vanek123@users.noreply.github.com>
2022-12-06 11:21:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+class Fruit {
+    // Properties
+    public $name;
+    public $color;
+  
+    // Methods
+    function set_name($name) {
+      $this->name = $name;
+    }
+    function get_name() {
+      return $this->name;
+    }
+    function set_color($color) {
+        $this->color = $color;
+    }
+    function get_color() {
+        return $this->color;
+    }
+  }
+  $apple = new Fruit();
+  $banana = new Fruit();
+  $apple->set_name('Apple');
+  $banana->set_name('Banana');
+  $apple->set_color('Red');
+  $banana->set_color('Yellow');
+
+  
+  echo "Name: " . $apple->get_name();
+  echo "<br>";
+  echo "Color: " . $apple->get_color();
+  echo "<br>";
+  echo "Name: " . $banana->get_name();
+  echo "<br>";
+  echo "Color: " . $banana-> get_color();
+?>
